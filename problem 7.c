f044228414f61f8ecc56aff5050b6b294449f5c4
Kavyah28 <231901021@rajalakshmi.edu.in>
2025-05-25 14:31:36 +0530

    Delete problem 7.c

--- problem 7.c
+++
@@ -1,9 +0,0 @@
-/* Write a C program to find the quotient and reminder of given integers. */
-
-#include <stdio.h>
-int main() {
-    int dividend, divisor;
-    scanf("%d %d", &dividend, &divisor);
-    printf("%d\n%d\n", dividend / divisor, dividend % divisor);
-    return 0;
-}
